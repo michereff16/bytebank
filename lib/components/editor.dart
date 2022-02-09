d6ab229309e8fe57a25435ed0297e6920d47ee0f
michereff16 <matheus_michereff@hotmail.com>
2022-02-09 18:19:32 -0300

    subindo

---
+++ lib/components/editor.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Editor extends StatelessWidget {
+  final TextEditingController? controlador;
+  final String? rotulo;
+  final String? dica;
+  final IconData? icone;
+
+  Editor({
+    this.controlador,
+    this.rotulo,
+    this.dica,
+    this.icone,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+          icon: icone != null
+              ? Icon(icone)
+              : null, //caso nao tiver icone, leva o texto pro canto da tela
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ).copyWith(
+          secondary: Colors.blueAccent[700],
+        ),
+        buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
+
+
